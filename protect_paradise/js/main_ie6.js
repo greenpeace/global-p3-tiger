502927502fdd95adbae5f235dff1284696d4e970
Alex Berger <alex@more-onion.com>
2013-10-15 17:06:54 +0200

    fallback for css nth-child selector

--- protect_paradise/js/main_ie6.js
+++ protect_paradise/js/main_ie6.js
@@ -1,6 +1,9 @@
 
 $(document).ready(function(){
 	if ($('html').hasClass('lt-ie9')) {
+    // fallback for css selector
+    $(".celebrities li:nth-child(n+4)").hide();
+
         $('.select label, .text label').css({
             'display': 'block'
         });
