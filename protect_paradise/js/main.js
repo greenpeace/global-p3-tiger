5995d24fb75859f4afdbf5b12a5ba9edc5e4ebbb
Alex Berger <alex@more-onion.com>
2013-10-15 15:43:00 +0200

    commented out left over console.log

--- protect_paradise/js/main.js
+++ protect_paradise/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	$("#js-show-more").click(function(){
 			if(i<max) i++; else i = 1;
 			if(j<max) j++; else j = 1;
-		  console.log('show:'+i+'; hide:'+j);
+		  //console.log('show:'+i+'; hide:'+j); // console.log is a big NO-NO for IE < 8
 			$(".celebrities li:nth-child("+j+")").fadeOut("fast");
 			$(".celebrities li:nth-child("+i+")").fadeIn("fast");
 
