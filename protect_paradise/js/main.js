1501744596533dc1e954f2c5db6612ed5be0796b
Alex Berger <alex@more-onion.com>
2013-10-16 18:38:34 +0200

    re-enable console guard code

--- protect_paradise/js/main.js
+++ protect_paradise/js/main.js
@@ -1,3 +1,25 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    'use strict';
+    var method,
+    noop = function() {},
+    methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'],
+    l = methods.length,
+    console = (window.console = window.console || {});
+
+    while (l--) {
+        method = methods[l];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
 
 $(document).ready(function(){
 	$('#section-container1').parallax("50%", 0.1);
