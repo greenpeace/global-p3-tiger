0fd575d86bfdd4cfde3f254ebd48602d177158a0
Alex Berger <alex@more-onion.com>
2013-10-16 16:49:44 +0200

    guard code for console errors (from gpi)

--- protect_paradise/js/main.js
+++ protect_paradise/js/main.js
@@ -1,3 +1,26 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    'use strict';
+    var method,
+    noop = function() {},
+    methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'],
+    l = methods.length,
+    console = (window.console = window.console || {});
+
+    while (l--) {
+        method = methods[l];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
 
 $(document).ready(function(){
 	$('#section-container1').parallax("50%", 0.1);
@@ -11,7 +34,7 @@ $(document).ready(function(){
 	$("#js-show-more").click(function(){
 			if(i<max) i++; else i = 1;
 			if(j<max) j++; else j = 1;
-		  //console.log('show:'+i+'; hide:'+j); // console.log is a big NO-NO for IE < 8
+		  console.log('show:'+i+'; hide:'+j);
 			$(".celebrities li:nth-child("+j+")").fadeOut("fast");
 			$(".celebrities li:nth-child("+i+")").fadeIn("fast");
 
