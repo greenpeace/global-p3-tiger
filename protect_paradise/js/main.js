4c7112448d948ab94186872534fa2f07b3b7ae08
Alex Berger <alex@more-onion.com>
2013-10-23 16:32:32 +0200

    use consistent indentation

--- protect_paradise/js/main.js
+++ protect_paradise/js/main.js
@@ -1,57 +1,57 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-    'use strict';
-    var method,
-    noop = function() {},
-    methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'],
+  'use strict';
+  var method,
+  noop = function() {},
+  methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeStamp', 'trace', 'warn'],
     l = methods.length,
     console = (window.console = window.console || {});
 
     while (l--) {
-        method = methods[l];
+      method = methods[l];
 
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
+      // Only stub undefined methods.
+      if (!console[method]) {
+        console[method] = noop;
+      }
     }
 }());
 
 $(document).ready(function(){
-  
-	if (!$('html').hasClass('lt-ie7')) {
-		$('#section-container1').parallax("50%", 0.1);
-		$('#section-container2').parallax("50%", 0.2);
-		$('#section-container3').parallax("50%", 0.1);
-	}
 
-	var i = 3;
-	var j = 0;
-	var max = $(".celebrities li").size();
+  if (!$('html').hasClass('lt-ie7')) {
+    $('#section-container1').parallax("50%", 0.1);
+    $('#section-container2').parallax("50%", 0.2);
+    $('#section-container3').parallax("50%", 0.1);
+  }
 
-	$("#js-show-more").click(function(){
-			if(i<max) i++; else i = 1;
-			if(j<max) j++; else j = 1;
-		  //console.log('show:'+i+'; hide:'+j); // console.log is a big NO-NO for IE < 8
-			$(".celebrities li:nth-child("+j+")").fadeOut("fast");
-			$(".celebrities li:nth-child("+i+")").fadeIn("fast");
+  var i = 3;
+  var j = 0;
+  var max = $(".celebrities li").size();
 
-	});
-
-// IE6 PNG fix
-	if ($('html').hasClass('lt-ie7')) {
-        DD_belatedPNG.fix('img, div.error, i, a, .section-raster-container, .logo, .png_bg');
-    }
-
-// check radio-button "other amount"
-    $('div.input.text.amount input').focus(function(){
-      $('input[name=DonationAmount]').prop('checked', true);
+  $("#js-show-more").click(function(){
+    if(i<max) i++; else i = 1;
+    if(j<max) j++; else j = 1;
+    console.log('show:'+i+'; hide:'+j);
+    $(".celebrities li:nth-child("+j+")").slideUp("normal", function() {
+      $(".celebrities li:nth-child("+i+")").slideDown("fast");
     });
 
+  });
+
+  // IE6 PNG fix
+  if ($('html').hasClass('lt-ie9')) {
+    DD_belatedPNG.fix('img, div.error, i, a, .section-raster-container, .logo, .png_bg');
+  }
+
+  // check radio-button "other amount"
+  $('div.input.text.amount input').focus(function(){
+    $('input[name=DonationAmount]').prop('checked', true);
+  });
 
 });
 
--- protect_paradise/js/main_ie6.js
+++ protect_paradise/js/main_ie6.js
@@ -1,17 +1,66 @@
 
 $(document).ready(function () {
 
-    fixFormPosition();
+  fixFormPosition();
 
-    if ($('html').hasClass('lt-ie8')) {
-        $('#UserEmail, #UserFirstname, #UserLastname').parent().find('label').each(function(){
-            var text = $(this).text();
-            $(this).text(text + ' *');
-        });
+  if ($('html').hasClass('lt-ie8')) {
+    $('#UserEmail, #UserFirstname, #UserLastname').parent().find('label').each(function(){
+      var text = $(this).text();
+      $(this).text(text + ' *');
+    });
+  }
+  if ($('html').hasClass('lt-ie9')) {
+    // fallback for css selector
+    $(".celebrities li:nth-child(n+4)").hide();
+  }
+  // Validierung für >=IE9
+  if ($('html').hasClass('lt-ie10')) {
+
+    $('.select label, .text label').css({
+      'display': 'block'
+    });
+
+  }
+
+  //
+  window.validated = true;
+
+  // validate not empty
+  $('#UserEmail, #UserFirstname, #UserLastname').keyup(function () {
+    if ($(this).val() == '') {
+      $(this).parent().find('.error').css('background', 'none').remove();
+      $(this).after('<div class="error">This field is required!</div>');
+      $(this).css({
+        'border': '3px solid red'
+      });
+
+    } else {
+      $(this).css({
+        'border': '1px solid grey'
+      });
+      $(this).parent().find('.error').css('background', 'none').remove();
     }
-    if ($('html').hasClass('lt-ie9')) {
-        // fallback for css selector
-        $(".celebrities li:nth-child(n+4)").hide();
+    fixFormPosition();
+    return true;
+  });
+
+  //validate email-string-structure
+  $('#UserEmail').keyup(function() {
+    var email = $('#UserEmail').val();
+
+    if (email.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/) == null) {
+
+      $(this).after('<div class="error">Please enter a valid email address!</div>');
+      $(this).css({
+        'border': '3px solid red'
+      });
+
+    } else   {
+      $(this).css({
+        'border': '1px solid grey'
+      });
+      $(this).parent().find('.error').css('background', 'none').remove();
+      return true;
     }
     // Label und Validierung für <=IE9
     if ($('html').hasClass('lt-ie10')) {
@@ -28,90 +77,44 @@ $(document).ready(function () {
         });
 
 
+  $('#action-submit-full input').click(function (event) {
+    $('#UserEmail, #UserFirstname, #UserLastname').keyup();
+    $('.error').first().parent().find('input').focus();
+    // check summary of all required fields
+    if ($('.error').length > 0) {
+      return false;
+    } else {
+      return true;
     }
-
-//
-            window.validated = true;
-
-            // validate not empty
-            $('#UserEmail, #UserFirstname, #UserLastname').keyup(function () {
-                if ($(this).val() == '') {
-                    $(this).parent().find('.error').css('background', 'none').remove();
-                    $(this).after('<div class="error">This field is required!</div>');
-                    $(this).css({
-                        'border': '3px solid red'
-                    });
-
-                } else {
-                    $(this).css({
-                        'border': '1px solid grey'
-                    });
-                    $(this).parent().find('.error').css('background', 'none').remove();
-                }
-//                alert('blur');
-                fixFormPosition();
-                return true;
-            });
-
-            //validate email-string-structure
-        $('#UserEmail').keyup(function() {
-            var email = $('#UserEmail').val();
-
-                if (email.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/) == null) {
-
-                    $(this).after('<div class="error">Please enter a valid email address!</div>');
-                    $(this).css({
-                        'border': '3px solid red'
-                    });
-
-                } else   {
-                $(this).css({
-                    'border': '1px solid grey'
-                });
-                $(this).parent().find('.error').css('background', 'none').remove();
-                return true;
-            }
-        });
+  });
 
 
-        $('#action-submit-full input').click(function (event) {
-            $('#UserEmail, #UserFirstname, #UserLastname').keyup();
-            $('.error').first().parent().find('input').focus();
-          // check summary of all required fields
-            if ($('.error').length > 0) {
-                return false;
-            } else {
-                return true;
-            }
-        });
+  $('div.input.text.amount input').keyup(function() {
+    var otherAmount = $('div.input.text.amount input').val();
 
+    if (isNaN(otherAmount)) {
+      $(this).parent().find('.error').css('background', 'none').remove();
+      $(this).after('<div class="error">Please enter a valid number!</div>');
+      $(this).css({
+        'border': '3px solid red'
+      });
 
-    $('div.input.text.amount input').keyup(function() {
-        var otherAmount = $('div.input.text.amount input').val();
+    } else   {
+      $(this).css({
+        'border': '1px solid grey'
+      });
+      $(this).parent().find('.error').css('background', 'none').remove();
+      return true;
+    }
+  });
 
-        if (isNaN(otherAmount)) {
-            $(this).parent().find('.error').css('background', 'none').remove();
-            $(this).after('<div class="error">Please enter a valid number!</div>');
-            $(this).css({
-                'border': '3px solid red'
-            });
+  // für donation.html: validate "other amount" field
+  if ($('input[name=DonationAmount]').length > 0 ) {
 
-        } else   {
-            $(this).css({
-                'border': '1px solid grey'
-            });
-            $(this).parent().find('.error').css('background', 'none').remove();
-            return true;
-        }
+    $('#donate-form-tiger').submit(function(){
+      $('div.input.text.amount input').keyup();
     });
-
-    // für donation.html: validate "other amount" field
-    if ($('input[name=DonationAmount]').length > 0 ) {
-
-        $('#donate-form-tiger').submit(function(){
-            $('div.input.text.amount input').keyup();
-        });
-    }
+  }
 
 
 });
@@ -123,10 +126,9 @@ function fixFormPosition() {
     var formOffset = $('#action-form.block').offset();
     var formHeight = $('#action-form.block').height();
     if (formOffset.top + 30 + formHeight < $(window).height()) {
-        $('#action-form').css({
-            'position':'fixed'
-        });
-//        alert('resize');
+      $('#action-form').css({
+        'position':'fixed'
+      });
     }
   }
 }
