c18140fe4ce2960a81db9d1702763de5d340d45b
funkygibbing <geek@raywalker.it>
2013-10-24 16:22:43 +1100

    Validation based on $.p3.validation.js
    Modified plugin to skip remote calls if jsonURL is false
    
    .... also snuck in some resize code for each parallax section because it looked ugly on my 1900x1200...

--- protect_paradise/js/main.js
+++ protect_paradise/js/main.js
@@ -21,8 +21,100 @@
     }
 }());
 
-$(document).ready(function(){
 
+/**!
+ * $.p3.request
+ *
+ * Query string parser, returns the url and an object
+ * containing the GET parameters in key: value format
+ *
+ * Required in most $.p3 libraries
+ *
+ * @author          <a href="mailto:hello@raywalker.it">Ray Walker</a>
+ * @requires        <a href="http://jquery.com/">jQuery</a>
+ * @usage           $.p3.request('http://fish.com?type=salmon');
+ * @version         0.1
+ * @param           {string} url jQuery
+ * @returns         {object} { url: 'http://fish.com', parameters: { type: 'salmon' } }
+ */
+(function($) {
+    'use strict';
+    var _p3 = $.p3 || {};
+
+    _p3.request = function(url) {
+        var request = {
+          url: false,
+          parameters: false
+        },
+        parts,
+        getRequestParams = function() {
+            var params = {};
+
+            if (parts[1]) {
+                parts[1].split(/[&;]/g).forEach(function (param) {
+                    var q = param.split(/\=/);
+                    if (q.length > 1 && q[0].length && q[1].length) {
+                        params[q[0]] = q[1];
+                    }
+                });
+            }
+            return params;
+        },
+        getRequestURL = function() {
+            return (parts[0].length) ? parts[0] : url;
+        };
+
+        if (url) {
+          parts = url.split('?');
+        } else {
+          return request;
+        }
+
+        request.url = getRequestURL(),
+        request.parameters = getRequestParams();
+
+        return request;
+    };
+
+    $.p3 = _p3;
+
+}(jQuery));
+
+$(document).ready(function() {
+  //
+
+  // Initialise parallax sizes
+  resizeSections();
+
+  $.p3.validation('#action-form', {
+    jsonURL: false,
+    rules: {
+      "firstname": {
+        "alphaPlus": true,
+        "required": true
+      },
+      "lastname": {
+        "alphaPlus": true,
+        "required": true,
+        "minlength": 3
+      },
+      "email": {
+        "required": true,
+        "email": true
+      },
+      "phone": {
+        "numeric":   true,
+        "minlength": 8
+      }
+    },
+    messages: {
+      "firstname":  "Please enter your first name",
+      "lastname":   "Please enter your last name",
+      "email":      "Please enter a valid email address",
+      "phone":      "Please enter your phone number",
+      "country":    "Please select your country"
+    }
+  });
   /* ----- initializing -------------------------- */
   // initialize form positioning, see below for further info
   fixFormPosition();
@@ -61,6 +153,9 @@ $(document).ready(function(){
   $(window).resize(function() {
     var $body = $('body');
 
+    // Resize each parallax section to fill the screen
+    resizeSections();
+
     // recalc form positioning
     fixFormPosition();
 
@@ -72,11 +167,15 @@ $(document).ready(function(){
 
       window.breakpointPassed = true;
     }
+
     // switch to mobile
     if (window.breakpointPassed && !$body.hasClass('desktop')) {
       // reset breakpoint indicator
       window.breakpointPassed = false;
     }
+
+
+
   });
 
   /* ------ IE specifics ----------------------- */
@@ -105,6 +204,19 @@ $(document).ready(function(){
   }
 });
 
+// Resize parallax sections to fill the screen
+window.tsw_resize_timer = false;
+function resizeSections() {
+  // debounce resize event
+  clearTimeout(window.tsw_resize_timer);
+  window.tsw_resize_timer = setTimeout(function () {
+    if ($('body').hasClass('desktop')) {
+      $('.desktop .section-container, .desktop .section-raster-container').height( $(window).height() - $('#header-container1').height() );
+    } else {
+      $('.tablet .section-container, .tablet .section-raster-container').attr('style','');
+    }
+  },50);
+}
 
 // check form-height against document-height -> position fixed or relative
 // this deals with the edge case when the form is too large to fit on a
--- protect_paradise/js/v03/p3.validation.js
+++ protect_paradise/js/v03/p3.validation.js
@@ -1,19 +1,21 @@
+
 /**!
  * p3.validation.js
- * 
- * @fileOverview Wrapper over the jquery.validation.js plugin for Greenpeace 
+ *
+ * @fileOverview Wrapper over the jquery.validation.js plugin for Greenpeace
  *               Action Template v03.
  *               Validates form data against XRegExp rules obtained via JSON endpoint
  * @author      <a href="mailto:hello@raywalker.it">Ray Walker</a>
- * @version     0.1
+ * @version     0.2.1
  * @copyright	Copyright 2013, Greenpeace International
  * @license	MIT License (opensource.org/licenses/MIT)
  * @requires    <a href="http://jquery.com/">jQuery 1.7+</a>,
  *              <a href="http://modernizr.com/">Modernizr</a>,
  *              <a href="http://xregexp.com/">XRegExp</a>
  *              <a href="http://jqueryvalidation.org/">jQuery Validate</a>
+ *              $.p3.request
  * @example     $.p3.validation('#action-form'[, options]);
- * 
+ *
  */
 /* global jQuery, Modernizr, XRegExp */
 (function($, M) {
@@ -25,7 +27,7 @@
         jsonURL: 'https://www.greenpeace.org/api/p3/pledge/config.json',
         tests: {
             // Matches all unicode alphanumeric characters, including accents
-            // plus . , - ' / 
+            // plus . , - ' /
             // Note for end users: when overriding or creating tests,
             // character strings must be double escaped: \\ instead of \
             // http://stackoverflow.com/questions/16572123/javascript-regex-invalid-range-in-character-class
@@ -42,27 +44,24 @@
         // Overrides jquery.validate default positioning
         errorPlacement: function(error, element) {
             $(element).parent().find('div.message').html(error);
-        }
+        },
+        // Query string parameters to include in validation request
+        params: {},
+        messageElement: '<div class="message"></div>'
     };
 
     _p3.validation = function(el, options) {
 
         var config = $.extend(true, defaults, options || {}),
-                getVars = {};
+        request = $.p3.request(config.jsonURL),
+        // Merge request GET variables from endpoint, defaults and config
+        query = {
+            url: request.url,
+            parameters: $.extend(true, request.parameters, config.params)
+        };
 
         if (config.showSummary) {
             config.summaryElement = $('.errorSummary', el).length ? $('.errorSummary', el) : $(el).prepend('<div class="errorSummary"></div>');
-        }
-
-        function parseGetVariables() {
-            var query = window.location.search.substring(1),
-                    vars = query.split('&'),
-                    obj = {};
-            for (var i = 0; i < vars.length; i++) {
-                var pair = vars[i].split('=');
-                obj[pair[0]] = pair[1];
-            }
-            return obj;
         }
 
         M.load({
@@ -72,67 +71,72 @@
             ],
             complete: function() {
                 var $el = $(el),
-                        $form = $el.is('form') ? $el : $('form', el);
+                $form = $el.is('form') ? $el : $('form', el);
 
                 if (config.loadGetParams) {
+                    // Obtain GET variables from the URL
+                    var getVars = $.p3.request(window.location.href).parameters;
+
                     // Populate form fields from GET variables
-                    getVars = parseGetVariables();
                     $.each(getVars, function(field, value) {
                         $('input[name=' + field + ']', $form).val(value);
                     });
                 }
 
-                $.getJSON(config.jsonURL, function(data) {
-                    var messageDiv = '<div class="message"></div>';
+                if (query.url) {
+                    // fetch rules from remote service
+                    $.getJSON(query.url, query.parameters, function(data) {
+                        $.extend(true, config, data || {});
+                    }).error(function() {
+                        // Failed to obtain JSON, fallback to html5 validation
+                        console.log('WARNING: JSON failed to load from: ' + config.jsonURL);
 
-                    $.extend(true, config, data || {});
-                    // Foreach data.tests ...
-                    $.each(config.tests, function(name, regexp) {
-                        // Don't trust the user entered data
-                        try {
-                            // Create a new validator method
-                            $.validator.addMethod(name, function(value, element) {
-//                                console.log('Testing '+value+' against '+regexp);
-                                var reg = new XRegExp(regexp);
-                                return this.optional(element) || reg.test(value);
-                            });
-//                            console.log("Added test '" +name + "': '" + regexp + "'");
-                        } catch (err) {
+                        if (config.fallbackHTML5) {
+                            // Disable validation plugin if can't load JSON
+                            console.log('WARNING: Using native HTML5 validation');
+                            return false;
+                        }
+                        // Else try to continue with existing rules...
+                    });
+                }
+
+                // Add custom tests
+                $.each(config.tests, function(name, regexp) {
+                    // Don't trust the user entered data
+                    try {
+                        // Create a new validator method
+                        $.validator.addMethod(name, function(value, element) {
+                            var reg = new XRegExp(regexp);
+                            return this.optional(element) || reg.test(value);
+                        });
+                    } catch (err) {
 //                            console.log("Failed to add test '" + name + "' with regex '" + regexp + "'");
-                        }
-
-                    });
-
-                    // Add message div to required fields
-                    // if it doesn't already exist in template
-                    $form.find('div.input.required').each(function() {
-                        var $this = $(this);
-
-                        if (!$this.find('div.message').length) {
-                            $this.append(messageDiv);
-                        }
-
-                    });
-                    // And finally go ahead and validate the form
-                    $form.validate(config);
-
-                    // Submit the form of auto_sign is true
-                    if (config.loadGetParams) {
-                        if (getVars.auto_sign == true) { // Yes i do mean == instead of === (allows either auto_sign=1 or auto_sign=true)
-                            $form.submit();
-                        }
                     }
-                }).error(function() {
-                    // Failed to obtain JSON, fallback to html5 validation
-                    console.log('WARNING: JSON failed to load from: ' + config.jsonURL);
-                    if (config.fallbackHTML5) {
-                        console.log('WARNING: Using native HTML5 validation');
-                    } else {
-                        $('input[type=submit]', el).attr('disabled', 'disabled').addClass('disabled');
-                        throw new Error('Form input disabled');
-                    }
+
                 });
 
+                var messageDiv = config.messageElement;
+
+                // Add message div to required fields
+                // if it doesn't already exist in template
+                $form.find('div.required').each(function() {
+                    var $this = $(this);
+
+                    if (!$this.find('div.message').length) {
+                        $this.append(messageDiv);
+                    }
+
+                });
+
+                // And finally go ahead and validate the form
+                $form.validate(config);
+
+                // Submit the form of auto_sign is true
+                if (config.loadGetParams) {
+                    if (getVars.auto_sign == true) { // Yes i do mean == instead of === (allows either auto_sign=1 or auto_sign=true)
+                        $form.submit();
+                    }
+                }
             }
         });
     };
